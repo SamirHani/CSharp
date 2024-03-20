0b8ea37c9febf56555d14f911a4718020d9997f8
samirhani <gabr22065@gmail.com>
2024-03-21 01:24:24 +0200

    that is all what i learned untill now

---
+++ CSharp.ConsoleApp/CSharp.ConsoleApp/Program.cs
@@ -0,0 +1,124 @@
+﻿namespace CSharp.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args);
+            Console.Title = "C SHARP IS AMAZING";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.Write("Hello, World!\n");
+            Console.WriteLine("this is new line");
+
+            // you have to wirte a value to the varible.
+            string sentenceToPrint = "welcome form the string variable";
+            Console.WriteLine(sentenceToPrint);
+
+            char letter = 'a';
+            Console.WriteLine(letter);
+
+            int old = 21;
+            Console.WriteLine(old);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+
+            long bigNumber = 90000000L;
+            Console.WriteLine(bigNumber);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+
+            double negative = -55.2D;
+            Console.WriteLine(negative);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+
+            float percision = 5.000001F;
+            Console.WriteLine(percision);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
+
+            decimal money = 14.99M;
+            Console.WriteLine(money);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(decimal.MinValue);
+
+            bool isMale = true;
+            Console.WriteLine(isMale);
+            isMale = false;
+            Console.WriteLine(isMale);
+
+            // string to integer
+            string convTo32 = "10";
+            int converted32 = Convert.ToInt32(convTo32);
+            Console.WriteLine(converted32);
+
+            string convTo64 = "46464646464646";
+            long converted64 = Convert.ToInt64(convTo64);
+            Console.WriteLine(converted64);
+
+            string textPersition = "5.000001";
+            float convertedFloat = Convert.ToSingle(textPersition);
+            Console.WriteLine(convertedFloat);
+
+            string textDouble = "5.0000001";
+            double convertedDouble = Convert.ToDouble(textDouble);
+            Console.WriteLine(convertedDouble);
+
+            string textDecimal = "14.99";
+            decimal convertedDecimal = Convert.ToDecimal(textDecimal);
+            Console.WriteLine(convertedDecimal);
+            Console.WriteLine("14.99");
+
+
+            // string concatination and number operations
+            int a = 0;
+            // postincrement
+            Console.WriteLine(a++); ;
+            // preincrement
+            Console.WriteLine(++a); ;
+            a = a + 10;
+            a += 10;
+            Console.WriteLine(a);
+
+            // it is the same like js you can use + - * / 
+
+            // you can not subtract a text or multiply it just + for concatination
+            string text = "concat";
+            text += "ination";
+            Console.WriteLine(text);
+
+            // when we concat two chars we get the sum of there hex code or ascii code don't remember exactly
+            char ch = 'a';
+            ch += 'b';
+            Console.WriteLine(ch);
+
+            // we also have the modulas %
+            Console.WriteLine(99%10);
+            Console.WriteLine(98 % 2);
+
+            // var keyword it seems like typeless 
+
+            var number = "0112548754";
+            Console.WriteLine(number);
+
+            // const
+            const string email = "gabr22065@gamil.com";
+            Console.WriteLine(email);
+
+            // read input from the user and store it in a variable to use it later
+            Console.Write("enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("enter your age: ");
+            string ageInput = Console.ReadLine();
+
+            int age = Convert.ToInt32(ageInput);
+
+            Console.WriteLine();
+            Console.WriteLine("Your name is " + name + " and your age is " + ++age );
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Forloop/Forloop/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forloop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            // while and if and do while and swithch
+            Console.Write("write the number of the day: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Friday");
+                    break;
+                case 2:
+                    Console.WriteLine("saterday");
+                    break;
+                case 3:
+                    Console.WriteLine("monday");
+                    break;
+                case 4:
+                    Console.WriteLine("tusday");
+                    break;
+                default: Console.WriteLine("invalid number");
+                    break;
+            }
+
+
+
+                Console.Write("What do you wanna repeat: ");
+                string repeatedText = Console.ReadLine();
+
+                Console.Write($"How many times you want to say {repeatedText}: ");
+                int times = Convert.ToInt32(Console.ReadLine());
+
+
+                if (times <= 0)
+                {
+                    Console.WriteLine("write a number above 0");
+                }
+                else
+                {
+                    for (int i = 0; i < times; ++i)
+                    {
+                        Console.WriteLine(repeatedText);
+                    }
+                }
+
+
+
+
+                do
+                {
+                    Console.WriteLine(times);
+                    times++;
+                } while (times < 10);
+
+
+
+                int myAge = 10;
+                // ternary if 
+                string result = myAge >= 0 ? "valid" : "invalid";
+                Console.WriteLine(result);
+
+
+            // Numeric Formating
+
+            double value = 10D / 3D;
+            Console.WriteLine(value);
+            Console.WriteLine(string.Format("{0:0}", value));
+            Console.WriteLine(string.Format("{0:0.0}", value));
+            Console.WriteLine(string.Format("{0:0.00}", value));
+            Console.WriteLine(string.Format("{0} {1}", value , "100"));
+            Console.WriteLine(string.Format("{0:0.#}", value)); // if the number is zero will remove it but if 1 will remain
+            Console.WriteLine(value.ToString("C0" ,CultureInfo.CreateSpecificCulture("en-US")));
+
+            Console.WriteLine();
+
+            bool success;
+            do
+            {
+                Console.Write("Create a id number: ");
+                string inputUserNumber = Console.ReadLine();
+                success = int.TryParse(inputUserNumber, out int number);
+                    if (success)
+                    {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                        success = true;
+                        Console.WriteLine($"welcome here is your id:{number}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("invalid number");
+                        Console.WriteLine();
+                    }
+            } while (success != true);
+
+            //string shit = Console.ReadLine();
+            //Console.WriteLine(Convert.ToInt32(shit));
+
+            // you can insert @ before the text to escape all the escape charchters but the quote you should use double double quote;
+            string path = @"C:\usr""\""sharing\info";
+            Console.WriteLine(path);
+            Console.ReadLine();
+        }
+
+    }
+}
